8dc0a77513372e62f72ba9f7bb4bccd9b01e0baf
sandropa <sandroparadzik@gmail.com>
2024-09-14 23:58:37 +0200

    .

--- main.cpp
+++ main.cpp
@@ -3,8 +3,30 @@
 #include <vector>
 #include <cmath>
 #include <string>
+#include <utility>
+#include <algorithm>
+#include <set>
 
 using namespace std;
+
+struct Semicircle {
+    int x;
+    int y;
+    int radius;
+    bool bottom;
+    static int sweepline_x;
+
+    Semicircle(int x, int y, int radius, bool bottom) 
+        : x{x}, y{y}, radius{radius}, bottom{bottom} {}
+    
+    bool operator<(const Semicircle& other) const {
+        // we are using y coordinate
+        if (y != other.y) {
+            return y < other.y;
+        }
+        return bottom < other.bottom;
+    }
+};
 
 struct Circle {
     int x;
@@ -45,8 +67,47 @@ int slow_intersections(vector<Circle>& circles) {
     return num_intersections;
 }
 
-int main(void)
-{
+
+bool fast_intersections(vector<Circle>& circles) {
+    vector<pair<int, Circle>> events;
+    // int represents priority, that is x coordinate
+    // in the set we will add the circle if it is not there, otherwise we will remove it
+    for(Circle c : circles) {
+        events.push_back({c.x - c.radius, c});
+        events.push_back({c.x + c.radius, c});
+    }
+    sort(events.begin(), events.end(), [](const pair<int, Circle>& a, const pair<int, Circle>& b) {
+        return a.first < b.first;
+    });
+
+    // now events are properly sorted
+
+    set<Semicircle> active_semicircles;
+
+    for(auto event : events) {
+        Circle curr_c = event.second;
+        Semicircle curr_top_smc = Semicircle(curr_c.x, curr_c.y, curr_c.radius, false);
+        Semicircle curr_bottom_smc = Semicircle(curr_c.x, curr_c.y, curr_c.radius, true);
+        if(active_semicircles.count(curr_top_smc)) {
+            // this means we need to add both top and bottom
+            active_semicircles.erase(curr_bottom_smc);
+            active_semicircles.erase(curr_top_smc);
+
+            
+            // PROVJERITI SUSJEDE
+        } else {
+            // this means we need to remove both top and bottom
+            active_semicircles.insert(curr_bottom_smc);
+            active_semicircles.insert(curr_top_smc);
+
+            // PROVJERITI SUSJEDE
+        }
+    }
+
+    return false;
+}
+
+int main() { // int main(void) ??
     // Initialization
     const int screenWidth = 1600;
     const int screenHeight = 900;
@@ -55,6 +116,11 @@ int main(void)
     SetTargetFPS(60);
 
     vector<Circle> circles;
+
+    /*
+    We can identify circles just by their centers, and if we add bottom or top
+    for the semicircles, we can identify them too (we are solving inly the general case)
+    */
 
     for(int i = 0; i < 20; i++) {
         int x = GetRandomValue(0, screenWidth);
@@ -75,7 +141,8 @@ int main(void)
 
         if (IsKeyPressed(KEY_ENTER))
         {
-            cout<<"Number of intersections: "<<slow_intersections(circles);
+            cout<<"Number of intersections: "<<slow_intersections(circles)<<endl;
+            cout<<fast_intersections(circles)<<endl;
         }
     }
 
